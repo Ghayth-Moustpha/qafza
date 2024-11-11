77a1281a48f5f1243d460b91af8b45e536bf8ab5
ghayth.moustapha@lit-co.net <ghayth.moustpha@gmail.com>
2024-11-11 19:45:55 +0300

    update about

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -26,13 +26,7 @@ const aboutTexts = [
 const About: React.FC = () => {
   return (
     <> 
-      <motion.div 
-                initial={{ opacity: 0, y: 20 }} 
-                animate={{ opacity: 1, y: 0 }} 
-                transition={{ duration: 1 }} 
-              >
-                <HeroSection3/>
-              </motion.div> 
+    
       <section className="py-24 relative">
         <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
           <div className="w-full justify-start items-center gap-12 grid lg:grid-cols-2 grid-cols-1">
@@ -54,6 +48,7 @@ const About: React.FC = () => {
                 alt="about Us image" 
               />
             </div>
+            
             <motion.div 
               initial={{ opacity: 0, x: -20 }} 
               animate={{ opacity: 1, x: 0 }} 
@@ -93,9 +88,16 @@ const About: React.FC = () => {
             </motion.div>
 
           </div>
+         
         </div>
       </section>
-    
+      <motion.div 
+                initial={{ opacity: 0, y: 20 }} 
+                animate={{ opacity: 1, y: 0 }} 
+                transition={{ duration: 1 }} 
+              >
+                <HeroSection3/>
+              </motion.div> 
       <FAQ/>
     </>
   );
